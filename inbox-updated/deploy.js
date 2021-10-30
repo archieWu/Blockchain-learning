8e8f050093b9606c0afcaa48cb31194d614c7d4a
Archie <yij1471@gmail.com>
2021-10-30 16:13:14 +0800

    合約版本提升

--- inbox-updated/deploy.js
+++ inbox-updated/deploy.js
@@ -1,8 +1,9 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode} = require('./compile');
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
     'daughter all portion wrong cave hat wait blur someone agree display board',
     'https://rinkeby.infura.io/v3/14b6e708ef5d425a969414729a5cc0e9'
 );
@@ -14,18 +15,18 @@ const deploy = async () => {
 
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
+    const result = await new web3.eth.Contract(abi)
         .deploy({
-            data: bytecode,
+            data: evm.bytecode.object,
             arguments: ['Hi there!']
         })
         .send({
             gas: '1000000',
-            gasPrice: '5000000000',
             from: accounts[0]
         });
 
     console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
 
 deploy();
